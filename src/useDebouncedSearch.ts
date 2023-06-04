e8f3a67eb0015a99c49750ad5eddb6aa72874b01
Mohammad <mhossein796@gmail.com>
2023-06-04 11:36:32 +0330

    waits for the user to stop then triggers - v.1.0.5

--- src/useDebouncedSearch.ts
+++ src/useDebouncedSearch.ts
@@ -3,38 +3,32 @@ import { useState, useEffect } from "react";
 const useDebouncedSearch = (delay = 300) => {
   const [search, setSearch] = useState("");
   const [searched, setSearched] = useState("");
-  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | undefined>();
 
   useEffect(() => {
+    let timeoutId: NodeJS.Timeout | undefined;
+
+    const debounce = () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+
+      timeoutId = setTimeout(() => {
+        setSearched(search);
+      }, delay);
+    };
+
+    debounce();
+
     return () => {
       if (timeoutId) {
         clearTimeout(timeoutId);
       }
     };
-  }, [timeoutId]);
-
-  const debounce = (callback: () => void, ms: number) => {
-    let timeout: NodeJS.Timeout | undefined;
-    return () => {
-      if (timeout) {
-        clearTimeout(timeout);
-      }
-      timeout = setTimeout(callback, ms);
-    };
-  };
-
-  const handleDebouncedSearch = () => {
-    setSearched(search);
-  };
+  }, [search, delay]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
-
-  useEffect(() => {
-    const debouncedSearch = debounce(handleDebouncedSearch, delay);
-    debouncedSearch();
-  }, [search, delay]);
 
   return { search, searched, handleChange };
 };
